bb08c4b1e48197bc31b3d3399861028f38b5c1b3
Alex Gusev <alex@flancer64.com>
2016-10-11 10:26:47 +0300

    Access inner data as properties.

---
+++ test/unit/DataT020AccessPropsSimpleTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Flancer32\Lib;
+
+/**
+ * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+ */
+class DataT020AccessPropsSimpleTest
+    extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function test_010_asScalar()
+    {
+        $name = 'property';
+        $value = 'value';
+        $obj = new Data();
+        $obj->$name = $value;
+        $this->assertEquals($value, $obj->$name);
+    }
+
+    public function test_020_asObj()
+    {
+        $name = 'property';
+        $value = new \stdClass();
+        $obj = new Data();
+        $obj->$name = $value;
+        $this->assertEquals($value, $obj->$name);
+    }
+
+    public function test_030_asArray()
+    {
+        $name = 'property';
+        $value = ['value'];
+        $obj = new Data();
+        $obj->$name = $value;
+        $this->assertEquals($value, $obj->$name);
+    }
+
+    public function test_040_asDataObject()
+    {
+        $name = 'property';
+        $value = new Data('value');
+        $obj = new Data();
+        $obj->$name = $value;
+        $this->assertEquals($value, $obj->$name);
+    }
+
+}
